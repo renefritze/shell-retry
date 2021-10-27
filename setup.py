2d50387069eaf08bc4e765d3ce1415b90ac23665
René Fritze <rene.fritze@wwu.de>
2021-10-27 15:52:04 +0200

    format with black

--- setup.py
+++ setup.py
@@ -10,19 +10,19 @@ def read(*paths):
 
 
 setup(
-    name='shell-retry',
-    version='0.0.8',
-    packages=['shell_retry'],
-    install_requires=['backoff<2', 'packaging'],
-    url='https://github.com/strizhechenko/shell-retry',
-    license='MIT',
-    author='Oleg Strizhechenko',
-    author_email='oleg.strizhechenko@gmail.com',
-    description='Wrapper for call any utilities with retries until they succeed',
-    long_description=(read('README.rst')),
+    name="shell-retry",
+    version="0.0.8",
+    packages=["shell_retry"],
+    install_requires=["backoff<2", "packaging"],
+    url="https://github.com/strizhechenko/shell-retry",
+    license="MIT",
+    author="Oleg Strizhechenko",
+    author_email="oleg.strizhechenko@gmail.com",
+    description="Wrapper for call any utilities with retries until they succeed",
+    long_description=(read("README.rst")),
     entry_points={
-        'console_scripts': [
-            'shell-retry=shell_retry.__init__:main',
+        "console_scripts": [
+            "shell-retry=shell_retry.__init__:main",
         ],
     },
 )
--- shell_retry/__init__.py
+++ shell_retry/__init__.py
@@ -7,8 +7,8 @@ from subprocess import check_output, CalledProcessError
 
 import backoff
 
-if Version(backoff.__version__) >= Version('2'):
-    other_kwargs = {'raise_on_giveup': False}
+if Version(backoff.__version__) >= Version("2"):
+    other_kwargs = {"raise_on_giveup": False}
 else:
     other_kwargs = {}
 args = None
@@ -18,15 +18,24 @@ def setup_logging(args):
     log_format = "%(asctime)s %(levelname)s: %(message)s"
     level = logging.INFO if args.verbose else logging.WARNING
     logging.basicConfig(format=log_format, level=level)
-    logging.getLogger('backoff').addHandler(logging.StreamHandler())
+    logging.getLogger("backoff").addHandler(logging.StreamHandler())
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--retry-count', type=int, help='How many times to re-run cmd if it fails', default=10)
-    parser.add_argument('--verbose', help='Be verbose, write how many retries left and how long will we wait',
-                        action='store_true', default=False)
-    parser.add_argument("cmd", nargs='+', type=str, action='store')
+    parser.add_argument(
+        "--retry-count",
+        type=int,
+        help="How many times to re-run cmd if it fails",
+        default=10,
+    )
+    parser.add_argument(
+        "--verbose",
+        help="Be verbose, write how many retries left and how long will we wait",
+        action="store_true",
+        default=False,
+    )
+    parser.add_argument("cmd", nargs="+", type=str, action="store")
     return parser.parse_args()
 
 
@@ -34,10 +43,12 @@ def _max_tries():
     return args.retry_count + 1
 
 
-@backoff.on_exception(wait_gen=backoff.expo,
-                      exception=CalledProcessError,
-                      max_tries=_max_tries,
-                      **other_kwargs)
+@backoff.on_exception(
+    wait_gen=backoff.expo,
+    exception=CalledProcessError,
+    max_tries=_max_tries,
+    **other_kwargs
+)
 def _run(args, retry):
     logging.info("run {0}".format(args.cmd))
     return check_output(args.cmd)
@@ -57,5 +68,5 @@ def main():
     run(args)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
